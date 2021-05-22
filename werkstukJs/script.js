2d3fce1f1c51208ca4358b25c3de5cc2de52b55c
Siebe Geyskens <siebe.geyskens@student.ehb.be>
2021-05-22 15:59:54 +0200

    feat(algorithm): finished

--- werkstukJs/script.js
+++ werkstukJs/script.js
@@ -4,7 +4,51 @@ const boxes = [{
 }, {
     name: "BoseBox",
     stats: "WATERPROOF, UP TO 30 HOURS, STEREO, COMPACT, MAXVOLUME: 100"
+}, {
+    name: "AppleBox",
+    stats: "SUPER COMPATIBLE, QUICK CONNECT, UP TO 8 HOURS"
+}, {
+    name: "SonyBox",
+    stats: "UP TO 30 HOURS, FAST CHARGE, WATERPROOF"
+}, {
+    name: "LGBox",
+    stats: "WATERPROOF, UP TO 30 HOURS, MULTI COLOR LIGHTS, DUAL ACTION BASS"
+}, {
+    name: "GoogleBox",
+    stats: "VOICE MATCH, SURROUND MODE, HANGS ON WALL"
+}, {
+    name: "UeBoomBox",
+    stats: "WATERPROOF, 360 DEGREE SOUND, COMPATIBLE APP"
+}, {
+    name: "BeatsBox",
+    stats: "BUILT-IN SPEAKEPHONE, SMALL SIZE, STEREO"
 }]
+
+// This orting algorithm is not pure functional.
+// The less and greater arrays are side-effects.
+const quickSort = (unsorted) => {
+    if (unsorted.length < 2 ) {
+        return unsorted;
+    } else {
+        let pivot = unsorted[0];
+        let less = [];
+        let greater = [];
+        unsorted.splice(0, 1);
+        pivotValues(unsorted, pivot.name, less, greater);
+        return quickSort(less).concat(pivot).concat(quickSort(greater));
+    }
+}
+
+function pivotValues(array, pivot, less, greater) {
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        if (element.name.toUpperCase() <= pivot.toUpperCase()) {
+            less.push(element);
+        } else {
+            greater.push(element);
+        }
+    }
+}
 
 const powerInstructions = "Press the powerButton on the remote to turn the box on or off,";
 const volumeInstructions = "Press the volumeUpButton on the remote to increase the volume,";
@@ -23,7 +67,6 @@ const instructionManual = (powerInstructions) => {
 const powerInstructionManual = instructionManual(powerInstructions)
 const powerInstructionManualWithVolumeInstructions = powerInstructionManual(volumeInstructions)
 
-const boxInstructionManual = boxes.map(powerInstructionManualWithVolumeInstructions)
-
+const boxInstructionManual = quickSort(boxes).map(powerInstructionManualWithVolumeInstructions)
 
 console.log(boxInstructionManual);
